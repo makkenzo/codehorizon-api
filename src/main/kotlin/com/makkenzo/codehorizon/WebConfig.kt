6cf94937bc27402f53fcc1a02fd7f1b0e78890a7
makkenzo <nekgo2009@gmail.com>
2025-02-18 16:44:50 +0500

    ✨ [FEAT]: Add CORS configuration for *.makkenzo.com domain

---
+++ src/main/kotlin/com/makkenzo/codehorizon/WebConfig.kt
@@ -0,0 +1,16 @@
+package com.makkenzo.codehorizon.com.makkenzo.codehorizon
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("https://*.makkenzo.com")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
