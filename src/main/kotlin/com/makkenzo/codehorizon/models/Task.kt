fd965e8c897db01dba0a97f1cdf3251379498f16
makkenzo <nekgo2009@gmail.com>
2025-04-30 13:27:57 +0500

    ✨ (model): Adds `TaskType` enum and `options` & `taskType` fields to `Task` model.

--- src/main/kotlin/com/makkenzo/codehorizon/models/Task.kt
+++ src/main/kotlin/com/makkenzo/codehorizon/models/Task.kt
@@ -4,5 +4,13 @@ data class Task(
     val id: String,
     val description: String,
     val solution: String,
-    val tests: List<String> = emptyList()
+    val tests: List<String> = emptyList(),
+    val taskType: TaskType = TaskType.TEXT_INPUT,
+    val options: List<String>? = null
 )
+
+enum class TaskType {
+    TEXT_INPUT,
+    CODE_INPUT,
+    MULTIPLE_CHOICE
+}
